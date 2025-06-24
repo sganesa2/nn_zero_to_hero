fc8bc3d042d94ff19582a2b8fe331f09ae306c25
Srinivas Ganesan <srinivas16112001@gmail.com>
2025-06-24 11:47:33 +0400

    initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import pytest
+from src.micrograd.micrograd import add
+
+@pytest.mark.parametrize(
+    "inp1, inp2, expected",
+    [
+        (1,2,3)
+    ])
+def test_addition(inp1:int, inp2:int, expected:int)->None:
+    assert add(inp1, inp2) == expected
